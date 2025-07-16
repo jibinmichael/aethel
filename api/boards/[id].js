0bb4356c9b49dbb5dc14affa6cf4963c46609255
Jibin Michael <gibinmichael@gmail.com>
2025-07-16 22:10:07 +0530

    Fix API routes: Convert to CommonJS format for Vercel compatibility

--- api/boards/[id].js
+++ api/boards/[id].js
@@ -1,6 +1,6 @@
-import clientPromise from '../db';
+const clientPromise = require('../db');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { method, query } = req;
   const { id } = query;
 
